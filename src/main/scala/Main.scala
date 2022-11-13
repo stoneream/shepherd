1daf00efa2f53ce7577e64e087a7201f65c27862
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2022-11-13 21:24:36 +0900

    HTMLを吐けるようにした

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,18 @@
+import scala.annotation.{tailrec, targetName}
+
 @main def main(): Unit =
-  val v =
+  val dom =
     div id "parent" $ (
-      div id "child1" $ (
+      div id "child 1" $ (
         div id "child1-1",
         div id "child1-2"
       ),
-      div id "child2" $ (
+      div id "child 2" $ (
         div id "child2-1"
       )
     )
+
+  println(dom.toHTML())
 
   /*
   ホントはインデントで表現できたら良いのですが...?（もしかしてできる？
@@ -20,13 +24,32 @@
       div id "child2-1"
    */
 
-  println(v)
-
 case class Tag(name: String, attrs: Seq[Attr] = Nil, children: Seq[Tag] = Nil):
   def addAttr(attr: Attr): Tag = this.copy(attrs = attrs :+ attr)
   def addChild(child: Tag): Tag = this.copy(children = children :+ child)
+  def toHTML(depth: Int = 0): String = {
+    val indent = "  " * depth
+    val attrsStr = attrs.map(_.toHTML).mkString(" ")
+    val startTag = s"$indent<$name $attrsStr>"
+    val childrenStr = {
+      val temp = children.map(_.toHTML(depth + 1))
+      if (temp.isEmpty) {
+        temp.mkString("")
+      } else {
+        temp.mkString("\n")
+      }
+    }
+    val endTag = s"$indent</$name>"
+
+    (if (childrenStr.isEmpty) {
+       Seq(startTag, endTag)
+     } else {
+       Seq(startTag, childrenStr, endTag)
+     }).mkString("\n")
+  }
 
 extension (parent: Tag)
+  @targetName("addChild")
   def $(children: Tag*): Tag = children.foldLeft(parent) { case (p, child) => p.addChild(child) }
   def id(name: String): Tag = parent.addAttr(IdAttr(name))
   def cls(name: String): Tag = parent.addAttr(ClassAttr(name))
@@ -34,6 +57,7 @@ extension (parent: Tag)
 trait Attr:
   val name: String
   val value: String
+  def toHTML: String = s"$name=\"${TextUtil.escape(value)}\""
 
 case class IdAttr(value: String) extends Attr:
   val name = "id"
