568cba1a21ba56ba9f2dc3ee8459afb1637dabf5
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2022-12-12 00:06:54 +0900

    ミスってたのとメモ追加

--- src/main/scala/html/Element.scala
+++ src/main/scala/html/Element.scala
@@ -7,6 +7,7 @@ import scala.annotation.targetName
  */
 
 // addAttribute もっとどうにかならないものか...
+// children/attributesについて。Listは末尾への追加の計算量が線形に増える、ので、reverseして先頭に追加して再度reverse
 
 sealed trait Element {
   val name: String
@@ -15,25 +16,25 @@ sealed trait Element {
 
 object Element {
   case class Void(name: String, attributes: List[Attribute]) extends Element {
-    def addAttribute(attribute: Attribute): Void = copy(attributes = (attribute :: attributes).reverse)
+    def addAttribute(attribute: Attribute): Void = copy(attributes = (attribute :: attributes.reverse).reverse)
 
     @targetName("addAttribute(Void)")
     def :=(attribute: Attribute): Void = addAttribute(attribute)
   }
 
   case class EscapableRawText(name: String, attributes: List[Attribute], text: String) extends Element {
-    def addAttribute(attribute: Attribute): EscapableRawText = copy(attributes = (attribute :: attributes).reverse)
+    def addAttribute(attribute: Attribute): EscapableRawText = copy(attributes = (attribute :: attributes.reverse).reverse)
 
     @targetName("addAttribute(EscapableRawText)")
     def :=(attribute: Attribute): EscapableRawText = addAttribute(attribute)
   }
 
   case class Normal(name: String, attributes: List[Attribute], children: List[Element | Text]) extends Element {
-    def addAttribute(attribute: Attribute): Normal = copy(attributes = (attribute :: attributes).reverse)
+    def addAttribute(attribute: Attribute): Normal = copy(attributes = (attribute :: attributes.reverse).reverse)
 
-    def addChild(child: Element): Normal = copy(children = (child :: children).reverse)
+    def addChild(child: Element): Normal = copy(children = (child :: children.reverse).reverse)
 
-    def addChild(child: Text): Normal = copy(children = (child :: children).reverse)
+    def addChild(child: Text): Normal = copy(children = (child :: children.reverse).reverse)
 
     @targetName("addChild(Element)")
     def /(child: Element.Normal): Normal = addChild(child)
